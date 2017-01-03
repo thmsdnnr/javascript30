fee1fa05a4b88703c2a29d5359c6f5089a157db7
Thomas Danner <thmsdnnr@gmail.com>
2017-01-03 09:38:30 -0600

    used es6 syntax and queryselectorall to match solution

--- day1/app.js
+++ day1/app.js
@@ -1,35 +1,19 @@
-//document.getElementById('keys').
 window.onload = function() {
-window.addEventListener("keydown", playSound);
 
-var keys = document.getElementsByClassName('key');
-var sounds = document.getElementsByClassName('sounds');
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key=>key.addEventListener('transitionend', removeTransition));
+  window.addEventListener("keydown", playSound);
 
-var keyMap = {};
-var soundMap = {};
-
-for (var i=0;i<keys.length;i++) {
-  keyMap[(keys[i].dataset.key)] = i;
-  soundMap[(sounds[i].dataset.key)] = i;
-  keys[i].addEventListener('transitionend', removeTransition);
-}
-//keyMap is an object with key: KEYCODE VALUE value: INDEX IN ELEMENTS ARRAY
-function playSound(e) {
-  if(keyMap[e.keyCode]!==undefined) //e.keyCode is the KEY for keyMap that equals to the
-  {
-    var audio=(sounds[soundMap[e.keyCode]]);
-    (keys[keyMap[e.keyCode]]).classList.add('key-active'); //className+=" key-active"; //index in the keys array of the element
+  function playSound(e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!key) { return; }
+    key.classList.add('key-active');
     audio.currentTime=0
-    audio.play();//play sound
-    /*setTimeout(function()
-    {
-      (keys[keyMap[e.keyCode]]).classList.remove('key-active');//.className="key";
-    },50);*/
+    audio.play();
   }
-}
 
-function removeTransition(e) {
-  if (e.propertyName!== 'transform') { return; }
-  e.target.classList.remove('key-active');
-}
+  function removeTransition(e) {
+    return (e.propertyName==='transform') ? e.target.classList.remove('key-active') : null;
+  }
 }
