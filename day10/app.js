7768135bde76032fdba6e0403eec509afc3b6cf9
Thomas Danner <thmsdnnr@gmail.com>
2017-01-04 22:08:10 -0600

    now works with unchecking

--- day10/app.js
+++ day10/app.js
@@ -1,38 +1,55 @@
 window.onload = function() {
-  const items = document.querySelectorAll('div.item');
-
-  function clearChecks() { items.forEach(i=>i.children[0].checked=false); }
-
-  function checkYourself() {
-  const checked=(Array.from(items).filter((i)=>i.children[0].checked===true));
-  //obtain the index of each of the checked items
-  var indexes = checked.map(c=>Array.from(items).indexOf(c));
-  //Then just iterate over the elements and set checked
-  for (var i=indexes[0];i<indexes[1];i++)
-  {
-    //console.log(items[i].children[0].checked);
-    items[i].children[0].checked=true;
-  }
-}
-
-  //const checkBox = document.querySelectorAll('input[type=checkbox]');
-  //console.log(checkBox);
-  //window.alert('sup brah!');
+  const inputs = document.querySelectorAll('div.item input');
   var shiftDown = false;
-  var mostRecentCheckedIndex;
+  var lastCheckedIndex;
+  var nextToLastCheckedIndex;
+  var lastUncheckedIndex;
+  var nextToLastUncheckedIndex;
+  styleParentDivs();
 
-  function handleKeyDown(e){ if (e.keyCode===16) { shiftDown=true; } }
-  function handleKeyUp(e){ if (e.keyCode===16) { shiftDown=false; } }
-  function handleClick(e) {
-    console.log(e);
-    mostRecentCheckedIndex=Array.from(items).indexOf(e.target);
-    console.log(mostRecentCheckedIndex);
-    if (shiftDown) { checkYourself(); }
+  inputs[0].parentNode.classList.add('ohey');
+  console.dir(inputs[0].parentNode.classList);
+
+  function returnLowToHigh(a,b) {
+    var c;
+    if (a>b) { c=a; a=b; b=c; }
+    return [a,b];
   }
+
+  function styleParentDivs() { //and style those divs!
+    inputs.forEach(i=>(i.checked) ? i.parentNode.classList.add('darken') : i.parentNode.classList.remove('darken'));
+  }
+
+  function checkYourself(a,b,flag) { //before you...
+    var indices = returnLowToHigh(a,b);
+    console.log("indices: "+indices+"flag: "+flag);
+    for (var i=indices[0];i<indices[1];i++) { inputs[i].checked=flag; }//GOTTA CHECK EM ALL!
+  }
+
+  //clear all checked boxes button
+  document.querySelector('#clearBoxes').addEventListener('click',clearChecks);
+  function clearChecks() { inputs.forEach(i=>i.checked=false); styleParentDivs(); }
+
+  //event listeners for shift key and checkbox clickage
   window.addEventListener('keydown',handleKeyDown);
   window.addEventListener('keyup',handleKeyUp);
-  items.forEach(i=>addEventListener('click',handleClick));
+  function handleKeyDown(e){ if (e.keyCode===16) { shiftDown=true; } }
+  function handleKeyUp(e){ if (e.keyCode===16) { shiftDown=false; } }
 
-  const clearBoxes=document.querySelector('#clearBoxes');
-  clearBoxes.addEventListener('click',clearChecks);
+  inputs.forEach(i=>addEventListener('click',wreckYourself));
+  function wreckYourself(e) { //sorry, I had to.
+    console.log(e.target.checked); //true or false
+    if (e.target.type!=="checkbox") { return ; } // don't care about yr clicks brah
+    if (e.target.checked===true) {
+      nextToLastCheckedIndex=lastCheckedIndex;
+      lastCheckedIndex=Array.from(inputs).indexOf(e.target);
+      if (shiftDown) { checkYourself(nextToLastCheckedIndex,lastCheckedIndex,true); styleParentDivs(); }
+    }
+    if (e.target.checked===false) {
+      nextTolastUncheckedIndex=lastUncheckedIndex;
+      lastUncheckedIndex=Array.from(inputs).indexOf(e.target);
+      console.log(nextTolastUncheckedIndex+" "+"last: "+lastUncheckedIndex);
+      if (shiftDown) { checkYourself(nextTolastUncheckedIndex,lastUncheckedIndex,false); styleParentDivs(); }
+    }
+  }
 }
