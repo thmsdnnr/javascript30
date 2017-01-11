bf9d23b4d759d2c6a407b9f71a8b8c3aa8ec7568
Thomas Danner <thmsdnnr@gmail.com>
2017-01-11 15:29:21 -0600

    day26 final

--- day26/README.md
+++ day26/README.md
@@ -2,21 +2,78 @@
 ##Stripe inspired Navbar
 ====
 
+One tricky part of this figuring out how to make the nav feel responsive. I didn't want it to disappear when you moused over the tool tip, and I wanted the transition to be smoothed out when you hovered from link to link, but I also didn't want it to hang around forever. I put a flag on toolTipMouseOver as well as navBarMousesOver and then call a setTimeout on my mouseleave listeners wait a few hundred ms, and then check these flags. My mouseon listeners reset the flags as appropriate. While maybe not the most elegant solution, it certainly works and the UI feels nice. I'm not sure [ ] TODO? if there's as better way of doing this or not. Pure CSS? Prettier JS?
+
+Another tricky part was THE almighty divot:
+
+```css
+div.navbox::after {
+  content: '';
+  display: inline-block;
+  position: absolute;
+  top: 0%;
+  left: 25%;
+  margin-top: -7.5px;
+  width: 10px;
+  height: 10px;
+  border-left: 2px solid gold;
+  border-top: 2px solid gold;
+  opacity: 1;
+  background-color: rgba(255,255,255,0.9);
+  transform: rotate(45deg);
+}
+```
+
+Explanation: the left/top border combo here creates a *vertical* white arrow. The values given for weight and color of the borders should match those of the container that they're in (in my case, the rest of the div tooltip has border 2px solid gold) to make a continuous-looing effect.
+
+*Basically what the code does is:*
+
+0. using the css ::after pseudo-class:
+1. creates a square div
+2. positions it absolutely along the border the tooltip div using top/left/bottom/right (in this case, top/left). Positions it within the tooltip (so it sticks out) with a negative `margin-[SIDE THE ARROW IS ON]` property that's equal to roughly 75% of the div width/height. To be specific, it should be equal to div (side-length/2) * sqrt(2), so in this case 5*sqrt(2) which is 7.07.
+3. rotates it by 45 degrees so it's "pointy"
+4. Gives it the same background color as the enclosing div. Boom!
+
+Only potential drawbacks of this implementation: can't have different color "arrow" than the bg color of the div, because there's still half a box hanging around. It's handy though in that it's pure CSS, no images involved.
+
+```javascript
+function magicNavigation(e) {
+  console.log(e);
+  e.stopPropagation();
+  visible=true;
+  if(e.target.id==="") { return; } //NOT THE EVENTS YR LOOKING 4
+  d.innerHTML=null;
+  d.classList.remove('invisible');
+  let coords=e.target.getBoundingClientRect();
+  d.style.top=`${coords.height+50}px`;
+  d.style.left=`${coords.right-coords.width}px`;
+  d.style.width=`${coords.width*2}px`;
+  populateNav(e,coords);
+}
+```
+
+Obviously to extend this to left/right/bottom divot you just change the borders displayed as well as the absolute positioning and also the top-left-bottom-right values.
+
+###TODO: ^ make a table or something for this.
+
+Divots inspired/extended by this (http://www.cssportal.com/css-tooltip-generator/)[http://www.cssportal.com/css-tooltip-generator/]. This makes the "Stems" or the "Divots" that you see on the top/right/left/bottom of tooltips. Basically what it does is:
+
+1. Create a div of zero dimensions and append it with absolute positioning to the tooltip div that appears selectively on mouse hover.
+2. Set a negative margin so it projects from the side of the box.
+3. Use border-left/right/top/bottom to make an "all border" div appear.
+
+
 ###Stuff I always forget:
 
-* `d.style.transform=`translate(${e.clientX}px,${e.clientY}px)`;` the PX ON THE TRANSLATE!@
-
+* `d.style.transform=`translate(${e.clientX}px,${e.clientY}px)`;` the PX ON THE TRANSLATE!
 * flex-flow: row wrap;
-
-* Something's wrong with the kill-hover but we're getting there.
+[X] Something's wrong with the kill-hover but we're getting there.
 
 ###TODO:
 
-* .invisible and .navchild are now flexboxes!
-
-* Make .invisible a container using flexbox and the children use flex
-* More programmatic div assemblage and insert into div.invisible as child
-* Prettier "divot" at top of navbar
-* Slightly more responsive?
-
-* Right now there's a child div in the hidden div. maybe a better way of doign this.
+[X] Make .invisible a container using flexbox and the children use flex
+[X] More programmatic div assemblage and insert into div.invisible as child
+[X] Prettier "divot" at top of navbar
+[X] Slightly more responsive?
+[ ] extend tooltip code, maybe make a "generate a tooltip" page in JS to practice displaying raw code / manipulating vars
+[ ] fix up a few of the "Magic numbers" like in the aptly named magicNavigation() function:
--- day26/app.js
+++ day26/app.js
@@ -1,30 +1,50 @@
-//document.querySelector('div.nav').style.display='block'; HELL!
-
 window.onload = function() {
-  let visible=false;
+
+  let mouseOverNav=false;
+  let mouseOverToolTip=true;
 
   let links=document.querySelectorAll('a');
   let listItems = document.querySelectorAll('ul.menu li');
   console.log(links);
+
   //live
-  Array.from(links).forEach((l)=>l.addEventListener('mouseover',magicNavigation,{capture:false}));
-  Array.from(listItems).forEach((l)=>l.addEventListener('mouseover',magicNavigation,{capture:false}));
+  links.forEach((l)=>l.addEventListener('mouseover',magicNavigation,{capture:false}));
+  //listItems.forEach((l)=>l.addEventListener('mouseover',magicNavigation,{capture:false}));
+  //listItems.forEach((l)=>l.addEventListener('mouseout',function(){killNav();}));
   //kill
   //Array.from(links).forEach((l)=>l.addEventListener('mouseout',killNav,{capture:false}));
-  //Array.from(listItems).forEach((l)=>l.addEventListener('mouseout',killNav));
+  //listItems.forEach((l)=>l.addEventListener('mouseout',killNav));
 
   let d = document.createElement('div');
-  d.classList+='navBox invisible';
+  d.classList.add('navBox','invisible');
   document.body.append(d);
+  d.addEventListener('mouseover',function(){mouseOverToolTip=true;});
+  d.addEventListener('mouseout',function(){console.log('out of tooltip'); mouseOverToolTip=false; killNav();});
+
+  let nav=document.querySelector('nav');
+  links.forEach((l)=>l.addEventListener('mouseover',function(){mouseOverNav=true; console.log(mouseOverNav+"el");}));
+  //nav.addEventListener('mouseover',function(){mouseOverNav=true; console.log(mouseOverNav+"el");});
+  nav.addEventListener('mouseout',function(){mouseOverNav=false; killNav();});
+  function killNav() {
+    console.log("kill MON: "+mouseOverNav+"kill TT: "+mouseOverToolTip);
+    setTimeout(function() {
+      ((mouseOverNav)||mouseOverToolTip) ? 0 : d.classList.add('invisible');
+    },250);
+  }
+
+//TODO on window change dimensions just make stuff disapper
 
   function magicNavigation(e) {
+    console.log(e);
     e.stopPropagation();
     visible=true;
     if(e.target.id==="") { return; } //NOT THE EVENTS YR LOOKING 4
     d.innerHTML=null;
-    d.classList=Array.from(d.classList).filter((c)=>c!=='invisible'); //remove class invisible
+    d.classList.remove('invisible');
     let coords=e.target.getBoundingClientRect();
-    d.style.transform=`translate(${coords.left}px,${coords.top+coords.height}px)`;
+    d.style.top=`${coords.height+50}px`;
+    d.style.left=`${coords.right-coords.width}px`;
+    d.style.width=`${coords.width*2}px`;
     populateNav(e,coords);
   }
 
@@ -32,7 +52,7 @@ window.onload = function() {
     let popular;
     let childDiv = document.createElement('div');
     childDiv.classList+='navChild';
-    childDiv.style.maxWidth=coords.width*1.2; //scale by 1.2
+    childDiv.style.width=coords.width*1.2; //scale by 1.2
     switch(e.target.id){
       case "home": popular=home; break;
       case "status": popular=status; break;
@@ -43,23 +63,20 @@ window.onload = function() {
     childDiv.innerHTML=popular;
     d.append(childDiv);
   }
-
-  function killNav(e) {
-    e.stopPropagation();
+  /*
     if (visible) {
-      d.classList+='invisible';
+
       visible=false;
     }
     else { return; }
-    console.log('kill :');
     console.dir(e);
-    //document.querySelector('div.navChild').classList+='invisible';
-  }
+    d.classList.add('invisible');
+  }*/
 
   let home=`<h1>Stuff</h1><br />More Stuff<br />More stuff<br />More stuff`;
   let status=`<h1>HEADER</h1><p>PARAGRAPH</p><p>ANOTHER ONE</p>`;
   let tweets=`<p>Painting should do one thing.</p><p>It should put happiness in your heart.</p><p>Use your imagination, let it go.</p>
   <p>Painting should do one thing.</p><p>It should put happiness in your heart.</p><p>Use your imagination, let it go.</p>`;
-  let history=`I guess that would be considered a UFO. A big cotton ball in the sky. There are no mistakes. You can fix anything that happens. Work on one thing at a time. Don't get carried away - we have plenty of time. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen.`;
+  let history=`<table><tr><td>I guess that would be considered a UFO.</td></tr><tr><td>A big cotton ball in the sky. There are no mistakes. You can fix anything that happens.</td></tr><tr><td>Work on one thing at a time. Don't get carried away - we have plenty of time. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen.</td></tr></table>`;
   let contact=`<h1>HEADER</h1><p>PARAGRAPH</p><p>ANOTHER ONE</p>`;
 }
